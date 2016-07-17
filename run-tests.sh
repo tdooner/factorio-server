ab5e1ff552cb312cbb12988436534f187d16329a
Tom Dooner <tom.dooner@brigade.com>
2016-07-17 14:09:22 -0700

    Add kitchen-ec2 for travis

---
+++ run-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -euo pipefail
+
+tmpdir=$(mktemp -d -t test-kitchen.XXXX)
+trap "rm -rf $tmpdir" EXIT
+
+# remove the key headers and replace them so OpenSSL parses it correctly
+echo $AWS_SSH_PRIVATE_KEY |
+  sed -e 's/-----BEGIN RSA PRIVATE KEY----- //g' | \
+  sed -e 's/ -----END RSA PRIVATE KEY-----//g' | \
+  sed -n 'i\
+-----BEGIN RSA PRIVATE KEY-----
+;
+p;
+i\
+-----END RSA PRIVATE KEY-----
+' > $tmpdir/id_rsa
+
+export AWS_SSH_KEY_PATH="$tmpdir/id_rsa"
+
+chmod 0600 $AWS_SSH_KEY_PATH
+
+kitchen verify
