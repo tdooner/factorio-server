44f5f0ab660141b9b5679635573c2e3887255da4
Tom Dooner <tom.dooner@brigade.com>
2016-07-17 14:54:23 -0700

    Rejigger private key formatting to be more correct

--- run-tests.sh
+++ run-tests.sh
@@ -5,20 +5,16 @@ tmpdir=$(mktemp -d -t test-kitchen.XXXX)
 trap "rm -rf $tmpdir" EXIT
 
 # remove the key headers and replace them so OpenSSL parses it correctly
+export AWS_SSH_KEY_PATH="$tmpdir/id_rsa"
+echo '-----BEGIN RSA PRIVATE KEY-----' > $AWS_SSH_KEY_PATH
 echo $AWS_SSH_PRIVATE_KEY |
   sed -e 's/"//g' | \
   sed -e 's/-----BEGIN RSA PRIVATE KEY----- //g' | \
   sed -e 's/ -----END RSA PRIVATE KEY-----//g' | \
-  sed -n 'i\
------BEGIN RSA PRIVATE KEY-----
-;
-p;
-i\
------END RSA PRIVATE KEY-----
-' > $tmpdir/id_rsa
+  tr " " "\n" >> $tmpdir/id_rsa
+echo '-----END RSA PRIVATE KEY-----' >> $AWS_SSH_KEY_PATH
 
-export AWS_SSH_KEY_PATH="$tmpdir/id_rsa"
 chmod 0600 $AWS_SSH_KEY_PATH
 openssl sha -sha256 $AWS_SSH_KEY_PATH
 
-bundle exec kitchen verify -l debug
+bundle exec kitchen test
